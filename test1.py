851c8e961fb12dcbec3eccfc51a2a8b3caaf21fb
yangkong2 <ayeongyang64@gmail.com>
2025-03-08 10:30:13 +0900

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,42 @@
+#watch = 100000
+#a = 'pig'
+#b = 'dad'
+
+#print(a)
+#print(b)
+#print(watch)
+#print(a+b)
+#print(a+' '+b)
+
+#aa= 1
+#bb = 100
+
+#aa,bb = bb, aa
+#print(aa)
+#print(bb)
+
+#예제 입니다
+#a1 = [10, 20, 30]
+#b1 = a1
+#a1[0] = 20
+#print(b)
+
+#프린트 사용예제
+#print("Hello World")
+#print('Hello World')
+#print(1)
+#print("1")
+
+#c = 1
+#print("c="+str(c))
+#print("c=", c)
+#print("c=".format(c))
+
+day1 프로그램
+a = [10, 20, 30]
+b = 1
+c = 2
+print (a)
+print("a={}".format(a))
+print("b={}".format(b))
+print("a={}, b={}, c={}".format(a, b, c))
